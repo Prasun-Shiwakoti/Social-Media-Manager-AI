427d90e7b84b0162a704b4432d7b75f14cfb4c95
Hritik-Thapa <hritikthapa331@gmail.com>
2026-01-06 22:11:12 +0545

    perf: changed token time

--- server/server/settings.py
+++ server/server/settings.py
@@ -120,8 +120,8 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=60),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=120),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
 }
