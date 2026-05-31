7c97c572ad21b58897347d7c5fc361bfb87ebb7f
Raj-Timalsina <rajtimalsina2062@gmail.com>
2026-05-31 21:17:29 +0545

    feat: initialize React app with Redux persistence and global 401 interceptor

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,18 @@ import { PersistGate } from "redux-persist/integration/react";
 import App from "./App";
 import { store, persistor } from "./redux/store.js";
 import "./index.css";
+import axios from "axios";
+import { logout } from "./redux/authSlice.js";
+
+axios.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      store.dispatch(logout());
+    }
+    return Promise.reject(error);
+  }
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
